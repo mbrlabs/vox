0942d6957622193f7c8ddd13972216d26ba40d5b
Marcus Brummer <mbrlabs7@gmail.com>
2017-05-02 17:37:46 +0200

    Added license headers

--- glm/vec3_test.go
+++ glm/vec3_test.go
@@ -1,7 +1,20 @@
+// Copyright (c) 2017 Marcus Brummer.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package glm
 
 import "testing"
-import "github.com/mbrlabs/gocraft/assert"
+import "github.com/mbrlabs/vox/assert"
 
 func TestVector3Add(t *testing.T) {
 	v := &Vector3{0, 0, 0}
