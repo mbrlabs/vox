c6b8f5036847ffdbdc191edde6b282e2c4f8c06f
Marcus Brummer <mbrlabs7@gmail.com>
2017-05-04 21:31:40 +0200

    Sandbox

---
+++ sandbox/main.go
@@ -0,0 +1,45 @@
+// Copyright (c) 2017 Marcus Brummer.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/mbrlabs/vox"
+	"runtime"
+)
+
+const (
+	windowTitle  = "Vox Sandbox"
+	windowWidth  = 1024
+	windowHeight = 768
+)
+
+// ----------------------------------------------------------------------------
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	window := vox.NewWindow(&vox.WindowConfig{
+		Height:       windowHeight,
+		Width:        windowWidth,
+		Title:        windowTitle,
+		Resizable:    false,
+		Fullscreen:   false,
+		Vsync:        true,
+		HiddenCursor: false,
+	})
+
+	window.Start(&Sandbox{})
+}
---
+++ sandbox/sandbox.go
@@ -0,0 +1,89 @@
+// Copyright (c) 2017 Marcus Brummer.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/mbrlabs/vox"
+)
+
+type Sandbox struct {
+	fpsController *vox.FpsCameraController
+	cam           *vox.Camera
+	renderer      *vox.WorldRenderer
+	world         *vox.World
+
+	oldX, dx float32
+}
+
+func (s *Sandbox) Create() {
+	vox.Vox.AddKeyListener(s)
+
+	ratio := float32(windowWidth) / float32(windowHeight)
+	s.cam = vox.NewCamera(70, ratio, 0.01, 1000)
+	s.cam.Move(0, 0, 50)
+	s.cam.Update()
+
+	s.fpsController = vox.NewFpsController(s.cam)
+	vox.Vox.AddMouseListener(s.fpsController)
+	vox.Vox.AddKeyListener(s.fpsController)
+
+	s.world = vox.NewWorld()
+	s.world.BlockBank.AddType(&vox.BlockType{ID: 0x01, Color: vox.ColorRed.Copy()})
+	s.world.BlockBank.AddType(&vox.BlockType{ID: 0x02, Color: vox.ColorGreen.Copy()})
+	s.world.BlockBank.AddType(&vox.BlockType{ID: 0x03, Color: vox.ColorBlue.Copy()})
+	s.world.BlockBank.AddType(&vox.BlockType{ID: 0x04, Color: vox.ColorTeal.Copy()})
+	s.world.GenerateDebugWorld()
+
+	s.renderer = vox.NewWorldRenderer()
+
+	gl.Enable(gl.DEPTH_TEST)
+}
+
+func (s *Sandbox) Dispose() {
+	s.renderer.Dispose()
+}
+
+func (s *Sandbox) Update(delta float32) {
+	s.fpsController.Update(delta)
+}
+
+func (s *Sandbox) Render(delta float32) {
+	// clear window
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.ClearColor(0.95, 0.95, 0.95, 0.0)
+
+	// render world
+	s.renderer.Render(s.cam, s.world)
+}
+
+func (s *Sandbox) Resize(width, height int) {
+
+}
+
+func (s *Sandbox) KeyDown(key glfw.Key) bool {
+	if key == glfw.KeyEscape {
+		vox.Vox.Exit()
+	}
+	return false
+}
+
+func (s *Sandbox) KeyUp(key glfw.Key) bool {
+	return false
+}
+
+func (s *Sandbox) KeyPressed(key glfw.Key) bool {
+	return false
+}
